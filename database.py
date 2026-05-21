b93a3e5c75ef87136fc4e36674b0bf85df82dcd9
juaness1234 <jcardozoricardo24@correo.unicordoba.edu.co>
2026-05-21 01:17:58 -0500

    fix railway deploy

--- database.py
+++ database.py
@@ -12,7 +12,7 @@ load_dotenv()
 
 DB_CONFIG = {
     "host":      os.getenv("DB_HOST",     "localhost"),
-    "port":      int(os.getenv("DB_PORT", "3306")),
+    "port":      int(os.getenv("DB_PORT" or "3306")),
     "user":      os.getenv("DB_USER",     "root"),
     "password":  os.getenv("DB_PASSWORD", ""),
     "database":  os.getenv("DB_NAME",     "vetpredict"),
